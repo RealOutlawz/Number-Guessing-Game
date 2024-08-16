31f528481feee9bc33301902825ac3b4a0e2e987
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-16 13:39:17 -0500

    Began working on game, created a function to check if number is higher or lower than random number

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let input = document.getElementById("number");
 let inputButton = document.getElementById("enterButton");
 let resetButton = document.getElementById("resetButton");
-let result = document.getElementById("feedback");
+let higherOrLower = document.getElementById("highLowOrEqual");
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+let chances = 10;
+
+inputButton.addEventListener("click", () => {
+  if (input.value < randomNumber) {
+    higherOrLower.textContent = "Too low!";
+    chances--;
+  } else if (input.value > randomNumber) {
+    higherOrLower.textContent = "Too high!";
+    chances--;
+  } else if (input.value == randomNumber) {
+    higherOrLower.textContent = "Correct!";
+  }
+});
