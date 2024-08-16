0af14756d744b4a4985c2ca415b9e927a1bfdc08
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-16 13:16:19 -0500

    Finished basic struture of HTML file and began work on javascript files

---
+++ script.js
@@ -0,0 +1,6 @@
+let input = document.getElementById("number");
+let inputButton = document.getElementById("enterButton");
+let resetButton = document.getElementById("resetButton");
+let result = document.getElementById("feedback");
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
