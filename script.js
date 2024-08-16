69af3de8d4150301e33906201e71fe1eadb5df6e
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-16 14:22:36 -0500

    Fixed spelling and decided to use chances over attempts

--- script.js
+++ script.js
@@ -7,19 +7,24 @@ let chancesLeft = document.getElementById("chancesLeft");
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 let chances = 10;
-chancesLeft.textContent = `You have ${chances} attempts left.`;
+chancesLeft.textContent = `You have ${chances} chances left.`;
 
 inputButton.addEventListener("click", () => {
-  if (input.value < randomNumber) {
-    result.textContent = "Too low!";
-    chances--;
-  } else if (input.value > randomNumber) {
-    result.textContent = "Too high!";
-    chances--;
-  } else if (input.value == randomNumber) {
-    result.textContent = "Correct! You won!";
+  let convertedNumber = Number(input.value);
+  if (convertedNumber === NaN) {
+    result.textContent = "Please enter a number between 1 and 100.";
+  } else {
+    if (convertedNumber < randomNumber) {
+      result.textContent = "Too low!";
+      chances--;
+    } else if (convertedNumber > randomNumber) {
+      result.textContent = "Too high!";
+      chances--;
+    } else if (convertedNumber == randomNumber) {
+      result.textContent = "Correct! You won!";
+    }
   }
-  chancesLeft.textContent = `${chances} attempts left.`;
+  chancesLeft.textContent = `${chances} chances left.`;
   if (chances === 0) {
     result.textContent = `You lost! The correct number was ${randomNumber}.`;
     inputButton.disabled = true;
@@ -31,6 +36,6 @@ resetButton.addEventListener("click", () => {
   chances = 10;
   result.textContent = "";
   input.value = "";
-  chancesLeft.textContent = `Chances left: ${chances}`;
+  chancesLeft.textContent = `You have ${chances} chances left.`;
   inputButton.disabled = false;
 });
