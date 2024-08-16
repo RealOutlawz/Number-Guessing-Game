447c588974fadcf51c5a7584de2959a147f8e09d
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-16 16:02:32 -0500

    Added styles, am continuing to update

--- script.js
+++ script.js
@@ -8,26 +8,45 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 let chances = 10;
 chancesLeft.textContent = `You have ${chances} chances left.`;
+let span = document.createElement("span");
+span.textContent = "Start guessing!";
+result.appendChild(span);
 
 inputButton.addEventListener("click", () => {
   let convertedNumber = Number(input.value);
   if (convertedNumber === NaN) {
     result.textContent = "Please enter a number between 1 and 100.";
   } else {
+    while (result.firstChild) {
+      result.removeChild(result.firstChild);
+    }
+    span.textContent = "";
     if (convertedNumber < randomNumber) {
-      result.textContent = "Too low!";
+      span.style.color = "red";
+      span.textContent = "Too low!";
+      result.appendChild(span);
       chances--;
     } else if (convertedNumber > randomNumber) {
-      result.textContent = "Too high!";
+      span.style.color = "red";
+      span.textContent = "Too high!";
+      result.appendChild(span);
       chances--;
     } else if (convertedNumber == randomNumber) {
-      result.textContent = "Correct! You won!";
+      span.style.color = "green";
+      span.textContent = "Correct! You won!";
+      result.appendChild(span);
     }
   }
   chancesLeft.textContent = `${chances} chances left.`;
   if (chances === 0) {
-    result.textContent = `You lost! The correct number was ${randomNumber}.`;
+    while (result.firstChild) {
+      result.removeChild(result.firstChild);
+    }
+    span.style.color = "red";
+    span.textContent = `You lost! The correct number was ${randomNumber}.`;
+    input.disabled = true;
     inputButton.disabled = true;
+    result.appendChild(span);
   }
 });
 
@@ -38,4 +57,8 @@ resetButton.addEventListener("click", () => {
   input.value = "";
   chancesLeft.textContent = `You have ${chances} chances left.`;
   inputButton.disabled = false;
+  input.disabled = false;
+  span.style.color = "white";
+  span.textContent = "Start guessing!";
+  result.appendChild(span);
 });
