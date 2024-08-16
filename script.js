6ff4816fb3fad58103a0623d495d5bee8b1e05d5
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-16 14:11:57 -0500

    Game is fully functional with working reset button

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 let input = document.getElementById("number");
 let inputButton = document.getElementById("enterButton");
 let resetButton = document.getElementById("resetButton");
-let higherOrLower = document.getElementById("highLowOrEqual");
+let result = document.getElementById("feedback");
+let chancesLeft = document.getElementById("chancesLeft");
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 let chances = 10;
+chancesLeft.textContent = `You have ${chances} attempts left.`;
 
 inputButton.addEventListener("click", () => {
   if (input.value < randomNumber) {
-    higherOrLower.textContent = "Too low!";
+    result.textContent = "Too low!";
     chances--;
   } else if (input.value > randomNumber) {
-    higherOrLower.textContent = "Too high!";
+    result.textContent = "Too high!";
     chances--;
   } else if (input.value == randomNumber) {
-    higherOrLower.textContent = "Correct!";
+    result.textContent = "Correct! You won!";
   }
+  chancesLeft.textContent = `${chances} attempts left.`;
+  if (chances === 0) {
+    result.textContent = `You lost! The correct number was ${randomNumber}.`;
+    inputButton.disabled = true;
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  chances = 10;
+  result.textContent = "";
+  input.value = "";
+  chancesLeft.textContent = `Chances left: ${chances}`;
+  inputButton.disabled = false;
 });
